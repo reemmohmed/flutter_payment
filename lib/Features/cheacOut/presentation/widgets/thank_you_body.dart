55609db6582194e3abd1b4ce8b6c04b200638111
shimaahriaz <shimaahriaz@gmail.com>
2025-01-20 15:16:14 +0200

    bottomCircle in screen

--- lib/Features/cheacOut/presentation/widgets/thank_you_body.dart
+++ lib/Features/cheacOut/presentation/widgets/thank_you_body.dart
@@ -5,6 +5,31 @@ class ThankYouBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+                color: const Color(0xffD9D9D9),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16))),
+          ),
+          Positioned(
+            left: -20,
+            bottom: MediaQuery.of(context).size.height * .2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
+              right: -20,
+              bottom: MediaQuery.of(context).size.height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              ))
+        ],
+      ),
+    );
   }
 }
